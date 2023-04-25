eb1a3320d1877c67840f089f39a1ca2153754cc9
Rodolfo <rodolfoalvesmdo@gmail.com>
2023-04-25 02:03:51 -0300

    Auto select proposal items and faq js interactions

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
 const links = document.querySelectorAll(".header-menu a");
 
 function changeLinkColor(link) {
-  if(document.location.href.includes(link.href)) {
+  if (document.location.href.includes(link.href)) {
     link.classList.add("active");
   }
 }
-links.forEach(changeLinkColor)
+links.forEach(changeLinkColor);
 
 
 /******* Image Gallery *******/
@@ -21,7 +21,8 @@ function swapImage(event) {
   const clicadaAlt = event.currentTarget.getAttribute("alt");
 
   if (principalSrc !== clicadaSrc) {
-    imagemPrincipal.setAttribute("src", clicadaSrc);imagemPrincipal.setAttribute("alt", clicadaAlt);
+    imagemPrincipal.setAttribute("src", clicadaSrc);
+    imagemPrincipal.setAttribute("alt", clicadaAlt);
     event.currentTarget.setAttribute("src", principalSrc);
     event.currentTarget.setAttribute("alt", principalAlt);
   }
@@ -32,3 +33,37 @@ function clickEvent(image) {
 }
 
 images.forEach(clickEvent);
+
+
+/******* Auto Set Proposal Items *******/
+const urlParams = new URLSearchParams(location.search);
+
+function setProposalItems(param) {
+  const proposalItem = document.querySelector('[value="' + param + '"]');
+
+  if (proposalItem) {
+    proposalItem.checked = true;
+  }
+}
+
+urlParams.forEach(setProposalItems);
+
+
+/******* FAQ *******/
+const buttons = document.querySelectorAll(".faq button");
+
+function expandFaqAnswer(event) {
+  const faqButtonControls = event.currentTarget.getAttribute("aria-controls");
+  const faqAnswer = document.getElementById(faqButtonControls);
+
+  faqAnswer.classList.toggle("active-faq");
+
+  const faqActive = faqAnswer.classList.contains("active-faq");
+  event.currentTarget.setAttribute("aria-expanded", faqActive);
+}
+
+function faqClickEvent(button) {
+  button.addEventListener("click", expandFaqAnswer);
+}
+
+buttons.forEach(faqClickEvent);
